11576fde84af33934f23aafc2a335871e88fbd35
Chris Beltrán <chrisgonza0@gmail.com>
2019-10-01 22:47:30 -0600

    Se agregó la programación sobre Hola Mundo

---
+++ Ejercicio Lab04.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	
+	string concatenar;
+	
+	string string1 = "Hola";
+	
+	string string2 = " Mundo";
+	
+	concatenar = string1 + string2;
+	
+	cout<<concatenar<<endl;
+	return 0;
+}
+
